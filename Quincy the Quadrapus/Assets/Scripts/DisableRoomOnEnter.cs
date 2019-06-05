0e0dc2f73b7061f1d49745966642298990d45482
Alexander Dean <alexander.dean94@gmail.com>
2019-06-05 16:22:23 -0400

    First commit with fixed gitignore

---
+++ Quincy the Quadrapus/Assets/Scripts/DisableRoomOnEnter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableRoomOnEnter : MonoBehaviour
+{
+    private UnityEngine.Tilemaps.TilemapRenderer obj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj = GetComponent<UnityEngine.Tilemaps.TilemapRenderer>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player") {
+            obj.enabled = false;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player") {
+            obj.enabled = true;
+        }
+    }
+}
